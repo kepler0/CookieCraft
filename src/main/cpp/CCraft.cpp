f8b10916e5c236e5118fbcfe528e4014ba7046b8
Kepler Sticka-Jones <kbsj269@gmail.com>
2014-12-03 18:44:20 -0700

    Adjust C++ Source File for newly created Header.

--- src/main/cpp/CCraft.cpp
+++ src/main/cpp/CCraft.cpp
@@ -1,14 +1,15 @@
-#include "CCraft.h"
+#include "native.h"
 
-extern "C"
-JNIEXPORT void JNICALL Java_com_k2b6s9j_CCraft_CCraft_preInit(JNIEnv *env, jobject obj) {
-
-}
-
-JNIEXPORT void JNICALL Java_com_k2b6s9j_CCraft_CCraft_init(JNIEnv *env, jobject obj) {
+extern "C" {
+JNIEXPORT void JNICALL Java_com_k2b6s9j_ccraft_CCraft_preInit(JNIEnv *env, jobject obj1, jobject obj2) {
 
 }
 
-JNIEXPORT void JNICALL Java_com_k2b6s9j_CCraft_CCraft_postInit(JNIEnv *env, jobject obj) {
+JNIEXPORT void JNICALL Java_com_k2b6s9j_ccraft_CCraft_init(JNIEnv *env, jobject obj1, jobject obj2) {
 
+}
+
+JNIEXPORT void JNICALL Java_com_k2b6s9j_ccraft_CCraft_postInit(JNIEnv *env, jobject obj1, jobject obj2) {
+
+}
 }
