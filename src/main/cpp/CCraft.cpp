f1fbf438f704192c6329638a51bbd9b93a82d8ec
Kepler Sticka-Jones <kbsj269@gmail.com>
2014-12-03 17:52:33 -0700

    Basic C++ Source File.
    Most Likely Incredibly Broken.

---
+++ src/main/cpp/CCraft.cpp
@@ -0,0 +1,14 @@
+#include "CCraft.h"
+
+extern "C"
+JNIEXPORT void JNICALL Java_com_k2b6s9j_CCraft_CCraft_preInit(JNIEnv *env, jobject obj) {
+
+}
+
+JNIEXPORT void JNICALL Java_com_k2b6s9j_CCraft_CCraft_init(JNIEnv *env, jobject obj) {
+
+}
+
+JNIEXPORT void JNICALL Java_com_k2b6s9j_CCraft_CCraft_postInit(JNIEnv *env, jobject obj) {
+
+}
