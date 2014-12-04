9eff2985f123b10392bd9bdc01e52e13e40ad739
Kepler Sticka-Jones <kbsj269@gmail.com>
2014-12-03 23:04:04 -0700

    Well, I've figured out how to get classes.

--- src/main/cpp/CCraft.cpp
+++ src/main/cpp/CCraft.cpp
@@ -1,15 +1,18 @@
 #include "native.h"
 
 extern "C" {
-JNIEXPORT void JNICALL Java_com_k2b6s9j_ccraft_CCraft_preInit(JNIEnv *env, jobject obj1, jobject obj2) {
-
+JNIEXPORT void JNICALL Java_com_k2b6s9j_ccraft_CCraft_preInit(JNIEnv *env, jobject obj, jobject jevent) {
+  jclass Class = env->GetObjectClass(obj);
+  jclass Event = env->GetObjectClass(event);
 }
 
-JNIEXPORT void JNICALL Java_com_k2b6s9j_ccraft_CCraft_init(JNIEnv *env, jobject obj1, jobject obj2) {
-
+JNIEXPORT void JNICALL Java_com_k2b6s9j_ccraft_CCraft_init(JNIEnv *env, jobject obj, jobject jevent) {
+  jclass Class = env->GetObjectClass(obj);
+  jclass Event = env->GetObjectClass(event);
 }
 
-JNIEXPORT void JNICALL Java_com_k2b6s9j_ccraft_CCraft_postInit(JNIEnv *env, jobject obj1, jobject obj2) {
-
+JNIEXPORT void JNICALL Java_com_k2b6s9j_ccraft_CCraft_postInit(JNIEnv *env, jobject obj, jobject jevent) {
+  jclass Class = env->GetObjectClass(obj);
+  jclass Event = env->GetObjectClass(event);
 }
 }
