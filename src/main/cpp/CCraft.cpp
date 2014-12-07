d1526464d58071e1782e59ae0a5fa27313537e30
Kepler Sticka-Jones <kbsj269@gmail.com>
2014-12-06 20:02:10 -0700

    Did some things with object handling.
    And almost working logging, I hope.
    
    I'm really just commiting this because it's compiling.
    I think...

--- src/main/cpp/CCraft.cpp
+++ src/main/cpp/CCraft.cpp
@@ -2,17 +2,35 @@
 
 extern "C" {
 JNIEXPORT void JNICALL Java_com_k2b6s9j_ccraft_CCraft_preInit(JNIEnv *env, jobject obj, jobject jevent) {
+  /* This Class */
   jclass Class = env->GetObjectClass(obj);
-  jclass Event = env->GetObjectClass(event);
+
+  /* The Event (FMLPreInitializationEvent) Class */
+  jclass Event = env->GetObjectClass(jevent);
+
+  /* Gets the getModLog method */
+  jmethodID modLogID = env->GetMethodID(Event, "getModLog", "()Lorg/apache/logging/log4j/Logger");
+  if (modLogID == NULL) {
+    /* error handling */
+  }
+
+  /* Gets the Logger object from the getModLog method ID */
+  jobject Log = env->CallObjectMethod(obj, modLogID);
 }
 
 JNIEXPORT void JNICALL Java_com_k2b6s9j_ccraft_CCraft_init(JNIEnv *env, jobject obj, jobject jevent) {
+  /* This Class */
   jclass Class = env->GetObjectClass(obj);
-  jclass Event = env->GetObjectClass(event);
+
+  /* The Event (FMLInitializationEvent) Class */
+  jclass Event = env->GetObjectClass(jevent);
 }
 
 JNIEXPORT void JNICALL Java_com_k2b6s9j_ccraft_CCraft_postInit(JNIEnv *env, jobject obj, jobject jevent) {
+  /* This Class */
   jclass Class = env->GetObjectClass(obj);
-  jclass Event = env->GetObjectClass(event);
+
+  /* The Event (FMLPostInitialization) Class */
+  jclass Event = env->GetObjectClass(jevent);
 }
 }
